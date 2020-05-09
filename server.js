172679cba1f306659c49eeb009c58a8d29608612
Darshan <dbelani59@gmail.com>
2020-05-09 14:39:31 +0530

    PORT CHANGES

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const app = express();
-const port = 5252;
+const port = process.env.PORT || 5000;
 
 // Set public folder as root
 app.use(express.static('public'));
